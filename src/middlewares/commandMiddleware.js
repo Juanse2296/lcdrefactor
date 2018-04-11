08dd1f7cd6be1252277814494544e653ad882b0d
Juan Sebastian Reina <juan-2296@hotmail.es>
2018-04-11 18:49:33 -0500

    add middlewares

---
+++ src/middlewares/commandMiddleware.js
@@ -0,0 +1,47 @@
+import { isNumeric, isEmpty, isNumber  } from '../helpers';
+
+export const getParameters = command => {
+  if (!command.includes(",")) {
+    throw new Error(`Cadena ${command} no contiene caracter ,`);
+  }
+  return command.split(",");
+};
+
+export const validateParameters = (parameters, command) => {
+  if (parameters.length > 2) {
+    throw new Error(`Cadena ${command} contiene más caracter ,`);
+  }
+  if (parameters.length < 2) {
+    throw new Error(`Cadena ${command} no contiene los parametros requeridos`);
+  }
+};
+
+export const getDigitSize = commandParameter => {
+  if (isNumeric(commandParameter)) {
+    return parseFloat(commandParameter);
+  } else {
+    throw new Error(`Parametro Size [ ${commandParameter} ] no es un numero`);
+  }
+};
+
+export const validateDigitSize = digitSize => {
+  if (digitSize < 1 || digitSize > 10) {
+    throw new Error(
+      "El parametro size [" + digitSize + "] debe estar entre 1 y 10"
+    );
+  }
+};
+
+export const getDigitSpace = answer => {
+  if (isEmpty(answer)) {
+    throw new Error("Esta vacio");
+  }
+  const parseAnswer = parseFloat(answer);
+  if (!isNumber(parseAnswer)) {
+    throw new Error("Ingrese un numero por favor!");
+  }
+  if (!Number.isInteger(parseAnswer)) {
+    throw new Error("El comando no es un entero!");
+  }
+  return parseAnswer;
+};
---
+++ src/middlewares/consoleMessage.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+export const error = chalk.bold.red;
+export const warning = chalk.keyword('orange');
+export const message = chalk.bold.cyan;
+export const notify = chalk.bold.magenta;
